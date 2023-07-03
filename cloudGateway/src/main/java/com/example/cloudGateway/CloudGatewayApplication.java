93d45bda6bd4e7b8e2d41af067d6ac50ac08c6ec
ArturSzejna <artur.szejna@beesset.com>
2023-07-03 15:31:29 +0200

    CircuitBreaker - adding REDIS replenishRate and burstCapacity configuration.

--- cloudGateway/src/main/java/com/example/cloudGateway/CloudGatewayApplication.java
+++ cloudGateway/src/main/java/com/example/cloudGateway/CloudGatewayApplication.java
@@ -6,12 +6,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
+	}
+
+	@Bean
+	KeyResolver userKeySolver() {
+		return exchange -> Mono.just("userKey");
 	}
 
 	public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
